99bf6c34a8b60ca04a3e0e70df8c78ee6ca13091
sebastianquintana <55902577+sebastianquintana@users.noreply.github.com>
2020-02-04 20:37:33 -0800

    index.php
    almost the same then HTML but I break in pieces the code in 3 files

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include 'includes/header.php';?>
+ 
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/meaceandthedogs.jpg" class="desktop" alt="Seba and ace with dogs" />
+ <img src="images/meandmymother.jpg" class="phone" alt="sebastian with his mother" />
+</section>
+
+        <section class="pagrah">
+          
+        <p>Hello my name is Sebastian Quintana and I'm in the web development program, this is my 3rd Quater in Seattle Central and I'm very
+  excited about this class because one of my goal in this program is in the future a front end development with an ability to understand more
+   than one language also learn a backend language too
+ </p>
+ <p>I'm original from Valparaiso, Chile and I moved 11 years ago to this beautiful country and city, back when I was in Chile I studied engineer 
+ in statistics and back then I took programming classes using a very old language call Pascal. I didn't finish school because after 2 years and a half I moved to here and here we are.
+ </p>
+ <p>Also, I been married almost 3 years and very happy to have a partner and we really like to travel so we are now focused to finish my school and my wife is a sous chef at Matt's in the market so always is good to go to adventures and try new food and experiences
+ </p>
+ <p>Just now we came back from Hong Kong and Vietnam and before we were in Chile and Argentina, 2019 was amazing and start also classes here. </p>
+
+
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside class="rightcolumn">
+ <h3>Right Column</h3>
+ <img src="images/mewithdog.jpg" class="tablet" alt="Sebastian with princces Leia" />
+ <p>This is not my dog but always is good to show to the people to princces Leia</p>
+ <p>she is amazing and love to take tea with the neighbor ,Anne 7 yers old </p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php';?>
