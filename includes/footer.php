9f06c897a0cd199416813f001bb2ffd52462561b
sebastianquintana <55902577+sebastianquintana@users.noreply.github.com>
2020-02-07 21:12:23 -0800

    update valiator
    updated new valiator for both CSS and HTML.

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <!-- START Footer -->
 <footer class="footer">
-<p><small>&copy; 2018 - <?=date('Y')?> by <a href="contactme.php" target="_blank">Sebastian Quintana</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+<p><small>&copy; 2018 - <?=date('Y')?> by <a href="contactme.php" target="_blank">Sebastian Quintana</a>, All Rights Reserved ~ <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fwww.quintanigal.com%2Fweb120%2Faia.php" target="_blank">Valid HTML</a> ~ <a href=" https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fvalidator.w3.org%2Fnu%2F%3Fdoc%3Dhttp%253A%252F%252Fwww.quintanigal.com%252Fweb120%252Faia.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank">Valid CSS</a></small></p>
 </footer>
 <!-- END Footer --> 
 </main>
