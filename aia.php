7fbbfc6ee8b8d62b38b05e69c794c30d5e5160e9
sebastianquintana <55902577+sebastianquintana@users.noreply.github.com>
2020-02-07 21:17:08 -0800

    added aia.php project
    added this new page to the website

---
+++ aia.php
@@ -0,0 +1,120 @@
+<?php include 'includes/header.php';?>
+<!-- start the table here. -->
+
+
+<h1> Sebastian Quintana aia.php </h1>
+<table class="top-table">
+
+  <tr>
+    <th>Client's name and final decision maker</th>
+    <th>Kyle Jhonson</th>
+  </tr>
+
+  <tr>
+    <th> Client's business/website name</th>
+    <th> kylejohnsontrainer.com</th>
+  </tr>
+  <tr>
+    <th>Redesign or brand new site</th>
+    <th>Brand new site</th>
+  </tr>
+      
+  <tr>
+    <th>Type of website</th>
+    <th>Marketing</th>
+  </tr>
+
+  <tr>
+    <th>Client's goal</th>
+    <th>The goal of this wesite is to give people access to an online training platform for gym work outs. It is built for poeple who are unable to make it to a physical gym or wish to 
+      supplement their current work out routine. </th>
+
+  </tr>
+
+
+
+</table>
+
+<table class="bottom-table">
+<tr class= "tr-header">
+  <th>Audience</th>
+  <th>Issues, Concerns</th>
+  <th>Approach</th>
+</tr>
+
+  <tr>
+    <th><h2>Potential Clients:</h2>
+    This website is going to be easy to navigating with an easy access to the menu bar and to any age want to work and know more
+    about work routine, access to subscription to get more information about the websiteand pay method to use the whole platform.
+    </th>
+    <th>
+      <ul>
+        <li>Contact information for Kyle</li>
+        <li>Videos showing routine</li>
+        <li>Gym's schedule</li>
+        <li>Membership</li>
+      </ul>
+    </th>
+    <th>
+      <ul>
+        <li>Contact page with all information </li>
+        <li>upload videos to the platform that allow clients to supplement routine</li>
+        <li>Show schedule by week/month</li>
+        <li>Prices for gym and all personal training options</li>
+      </ul>
+    </th>
+  </tr>
+
+  <tr>
+    <th><h2>Current clients :</h2>
+    How Kyle's current clients can use the website to supplement their workout and improve techniques and lifestyle habits 
+    
+    </th>
+    <th>
+      <ul>
+        <li>Personalized routine</li>
+
+        <li>online appointment schedule</li>
+        <li>Health information</li>
+    </ul>
+  </th>
+    <th><ul>
+      <li>Personalized routine page to upload videos that are specific to clients need</li>
+      
+      <li>page for scheduling with your trainer by week or month</li>
+      <li>page for additional information about eating, stretching, resting etc to support your body while training </li>
+  </ul></th>
+  </tr>
+
+  <tr>
+    <th><h2>PROSPECTIVE EMPLOYEES:</h2>
+    Prospective personal trainers or gym support staff looking for a job with Kyle.
+    </th>
+    <th>
+      <ul>
+        <li>biography for kyle</li>
+        <li>Easy access to the site and schedules</li>
+        <li>information on prospective employees</li>
+        
+    </ul>
+</th>
+    <th>
+      <ul>
+      <li>background and contact information on kyle to understand his career and goals</li>
+      <li>menu and pages that give a good representation of the physical and online appearance of the gym</li>
+      <li>A page allowing prospective employees to answer questions on schedule availability and past experience</li>
+    
+  </ul>
+</th>
+  </tr>
+
+
+
+</table>
+
+
+
+
+
+ 
+<?php include 'includes/footer.php';?>
