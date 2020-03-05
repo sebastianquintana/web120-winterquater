09fe02e0a4cd0e30affb5f6d0919039141d14930
sebastianquintana <55902577+sebastianquintana@users.noreply.github.com>
2020-03-05 14:47:56 -0800

    add templete.php
    templete.php

---
+++ templete.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?=$title?></title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/templete.css" />
+</head>
+<body>
+<div class="topnav">
+              <a  href ="contact.html">Contact</a>
+              <a  href="service.html">trainning</a>
+              <a  href="about.html">About</a>
+              <a  class="active" href="index.html">Home</a>
+ </div>
+
+ <div class = "grid">
+     <div class = "one">
+         <h2>Beginner</h2>
+         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+             Vestibulum tortor quam, feugiat vitae,
+              ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi 
+              vitae est. Mauris placerat eleifend leo.</p>
+              <ul>
+   <li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
+   <li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
+   <li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
+   <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
+</ul>
+     </div>
+     <div class = "two">
+     <h2>Medium Routing</h2>
+         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+             Vestibulum tortor quam, feugiat vitae,
+              ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi 
+              vitae est. Mauris placerat eleifend leo.</p>
+              <ul>
+   <li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
+   <li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
+   <li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
+   <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
+</ul>
+     </div>
+     <div class = "three">
+     <h2>Pro Routing</h2>
+         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+             Vestibulum tortor quam, feugiat vitae,
+              ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi 
+              vitae est. Mauris placerat eleifend leo.</p>
+              <ul>
+   <li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
+   <li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
+   <li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
+   <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
+</ul>
+     </div>
+     <div class = "four">
+     <h1>Routing seasson</h1>
+     </div>
+     <div class = "five">
+     <h2>How to eat healty</h2>
+     <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies
+          eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit
+           amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt
+            condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus f
+            aucibus, tortor neque egestas augue, eu 
+         vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+ 
+     </div>
+     <div class = "six">
+
+     <video width="320" height="240" controls>
+  <source src="images/1280.mp4" type = "video/mp4">
+  </video>
+  <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, 
+      tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+<video width="320" height="240" controls>
+    <source src="images/1280.mp4" type = "video/mp4">
+    
+  </video>
+  <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, 
+    tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+<video width="320" height="240" controls>
+  <video width="320" height="240" controls>
+    <source src="images/1280.mp4" type = "video/mp4">
+    
+  </video>
+  <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, 
+    tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+<video width="320" height="240" controls>
+  <video width="320" height="240" controls>
+    <source src="images/1280.mp4" type = "video/mp4">
+    
+  </video>
+  <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, 
+    tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+<video width="320" height="240" controls>
+     </div>
+     <div class = "seven">"
+    <img src="images/test.jpg" type = "testGym">
+     </div>
+</div>
+<footer class="footer">
+    <p><small>&copy; 2018 - <?=date('Y')?> by <a href="contactme.php" target="_blank">Sebastian Quintana</a>, All Rights Reserved ~ <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fwww.quintanigal.com%2Fweb120%2Faia.php" target="_blank">Valid HTML</a> ~ <a href=" https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fvalidator.w3.org%2Fnu%2F%3Fdoc%3Dhttp%253A%252F%252Fwww.quintanigal.com%252Fweb120%252Faia.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank">Valid CSS</a></small></p>
+    </footer>
